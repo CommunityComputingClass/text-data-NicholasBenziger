bbec21637620a05d05bae01f4b60c6366fd99045
NicholasBenziger <26nichol.benziger@lwhs.org>
2025-02-10 23:27:08 +0000

    reload

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,46 @@
+let myQuestion = "what was the best part of you're week?"
+ 
+let contents = "";
+
+let button
+let textBox; 
+let refreshb
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
+  button = createButton("show");
+  button.position(220,80)
+  button.mousePressed(showText)
+  
+  textBox = createInput("type your answer here")
+  textBox.size(300,40)
+  textBox.position(50,200)
+  textBox.input(storeText)
+  
+  background(50);
+  fill(255);
+  textSize(14);
+  text(myQuestion,50,50) 
+
+  refreshb = createButton("reload");
+  refreshb.position(220,300)
+  refreshb.mousePressed(refresh)
 }
 
 function draw() {
-  background(200);
   
+}
+
+function showText(){ 
+   text(contents,50,200)
+   textBox.hide(); 
+   
+}
+
+function storeText(){
+  contents = this.value(); 
+}
+
+function refresh () {
+location.reload()
 }
