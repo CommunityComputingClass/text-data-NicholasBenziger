560c51a5d15d1b6395c41906b3e3cd04038ac81b
NicholasBenziger <26nichol.benziger@lwhs.org>
2025-03-11 04:06:00 +0000

    worksheet 1

--- project/worksheet1.js
+++ project/worksheet1.js
@@ -1,7 +1,27 @@
-function setup(){
-    createCanvas(200,200)
+let myFont; 
+let myMessage = "hey there"
+
+function preload(){
+  myFont = loadFont("splashpoint.otf")
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+ 
 }
 
 function draw(){
-    background("blue)
+    background(55);
+    fill('white');
+    quad(200, 80, 350, 200, 200, 310, 50, 200)
+    fill('pink')
+    square(125,120,150,50)
+    fill('red')
+    circle(200,195,100)
+    fill(255) 
+    textFont(myFont)
+    text(myMessage, 175, 200);
+   
+    
 }
