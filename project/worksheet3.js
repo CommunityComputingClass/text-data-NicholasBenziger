748185319dc34876334ed01cf52cb5576be8363b
NicholasBenziger <26nichol.benziger@lwhs.org>
2025-03-12 00:35:09 +0000

    worksheet 3

---
+++ project/worksheet3.js
@@ -0,0 +1,38 @@
+let myQuestion = "what was the best part of you're week?"
+ 
+let contents = "";
+
+let button
+let textBox; 
+
+function setup() {
+  createCanvas(500, 400);
+  button = createButton("show");
+  button.position(220,80)
+  button.mousePressed(showText)
+  
+  textBox = createInput("type your answer here")
+  textBox.size(300,40)
+  textBox.position(50,200)
+  textBox.input(storeText)
+  
+  background(50);
+  fill(255);
+  textSize(14);
+  text(myQuestion,50,50) 
+
+}
+
+function draw() {
+  
+}
+
+function showText(){ 
+   text(contents,50,200)
+   textBox.hide(); 
+   
+}
+
+function storeText(){
+  contents = this.value(); 
+}
